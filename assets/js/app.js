9fa12c383c1e0fc4baef2b20664cba74aa1d3855
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-01-20 22:38:28 -0200

    Início do desenvolvimento da lógica do uso da biblioteca zxcvbn no arquivo app.js

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+/**
+ * Arquivo: app.js
+ * Author: Glaucia Lemos
+ * Data: 20/01/2017
+ * 
+ * Description: Arquivo responsável por toda a lógica do projeto.
+ */
+
+(function(){
+    var ZXCVBN_SRC = 'bower_components/zxcvbn/dist/zxcvbn.js';
+
+    var async_load = function() {
+        var primeiroElemento, script;
+
+        /* Criando um elemento <script> para ser usado pelo DOM API*/
+        script = document.createElement('script');
+
+        /* Iniciar os atributos no elemento script */
+        script.src = ZXCVBN_SRC;
+        script.type = 'text/javascript';
+        script.async = true; //async é um atributo do HTML5
+
+        /* Agora aqui nós vamos pegar o primeiro elemento do script no documento */
+        primeiroElemento = document.getElementByTagName('script')[0];
+
+        /* Em seguida vamos inserir o elemento <script> antes do primeiro elemento no documento */
+        return primeiroElemento.parentNode.insertBefore(script, primeiroElemento);
+    };
+
+    /* Agora... vamos adicionar o async_load como callback para o evento window load */
+    if(window.attachElement != null) {
+        window.attachEvent('onload', async_load);
+    } else {
+        window.addEventListener('load', async_load, false);
+    }
+}).call(this);
